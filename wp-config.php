fa988b2c58927ca518ec3e63affa2aec605c6f35
dwheiz_Hat <80315603+matt-64@users.noreply.github.com>
2022-10-24 12:24:32 +0200

    upgrade wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -83,7 +83,7 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-
+define('WP_MEMORY_LIMIT', '96M');
 
 /* That's all, stop editing! Happy publishing. */
 
